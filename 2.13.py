47bd241e5d3f0071bcc002816e0b194976023bb0
votahoang090398 <61549383+votahoang090398@users.noreply.github.com>
2020-05-11 21:37:57 +0700

    Add files via upload

---
+++ 2.13.py
@@ -0,0 +1,15 @@
+a=int(input("nhap a"))
+b=int(input("nhap b"))
+c=int(input("nhap c"))
+deta= b*b-4*a*c;
+if a==0:
+ print("pt vo nghiem")
+else:
+ if deta<0:
+  print("pt vo nghiem")
+ else:
+  if deta==0:
+   print("co nghiem kep")
+  else:
+   print("co 2 nghiem phan biet")
+ 
